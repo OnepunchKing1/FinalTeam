8bd8e00ec98fe537519452f16c880834a03ba4e4
Daeseong Oh <abr00122@naver.com>
2023-07-22 21:24:36 +0900

    Revert "Test"
    This reverts commit ea5a03615e8174c52e38e224bf8a2fb30a3e4f27.

--- Tool/Public/Level_Loading_Tool.h
+++ Tool/Public/Level_Loading_Tool.h
@@ -18,7 +18,6 @@ public:
 private:
 	LEVELID				m_eNextLevelID = { LEVEL_END };
 	class CLoader_Tool* m_pLoader = { nullptr };
-	_int				Test = 0;
 
 public:
 	static			CLevel_Loading_Tool* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, LEVELID eNextLevelID);
